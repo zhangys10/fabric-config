633115f880f07bb37bf917cb318bc47693497749
Ryan Zhang <zhangys10@hotmail.com>
2018-08-07 15:48:50 +0800

    First commit

---
+++ SpringCloudDemo-config/src/main/java/org/fabric/demo/SpringCloudDemoConfigApplication.java
@@ -0,0 +1,14 @@
+package org.fabric.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudDemoConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudDemoConfigApplication.class, args);
+	}
+}
